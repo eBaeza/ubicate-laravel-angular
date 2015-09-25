6edd685f5053e55d72288089920889bc8ad3ee11
Edgar A. Baeza <ebaeza1992@gmail.com>
2015-09-25 05:31:25 -0500

    Create model and migrations of careers

---
+++ database/migrations/2015_09_25_101737_create_careers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCareersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('careers', function (Blueprint $table) {
+            $table->increments('id_career');
+            $table->string('name_career');
+            $table->string('area_career');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('careers');
+    }
+}
