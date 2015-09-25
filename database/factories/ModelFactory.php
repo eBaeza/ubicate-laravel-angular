240be2b8b6c7390c9843a327fa12e78f7e748c79
Edgar A. Baeza <ebaeza1992@gmail.com>
2015-09-25 06:09:28 -0500

    model factory and seeder for table careers

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,15 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+// factory for Careers
+$factory->define(App\Career::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
+        'name_career' => $faker->bs,
+        'area_career' => $faker->randomElement([
+        	'Aeronáutica',
+        	'Educación',
+        	'Computación',
+        	'Medicina'
+        ])
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        // seeder for careers
+        $this->call(CareersTableSeeder::class);
 
         Model::reguard();
     }
